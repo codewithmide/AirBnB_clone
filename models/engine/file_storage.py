d207efa99bf31b9148c9db7101a4eecdeb2d82f4
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-01 12:03:43 -0700

    package[engine]: added the reload and save methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,16 +19,25 @@ class FileStorage:
 
     def all(self):
         """
-        return all
+        returns the dictionary __objects
         """
         return self.__objects
 
     def new(self, obj):
-        # BaseModel.12121212
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+# BaseModel.12121212
         key = f"{type(obj).__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
         class_dict = {
             "BaseModel": BaseModel,
         }
@@ -41,3 +50,13 @@ class FileStorage:
                     self.new(class_dict[class_name](**value))
         except FileNotFoundError:
             pass
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        json_obj = self.__objects.copy()
+        for key, val in self.__objects.items():
+            json_obj[key] = val.to_dict()
+        with open(self.__file_path, 'w', encoding='UTF-8') as file:
+            file.write(json.dumps(json_obj))
