d809c651bac772f8242698af13bfcdff7f20e1ae
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-02 10:51:26 -0700

    made some changes to init and console

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,11 @@
 """
 A unique FileStorage instance for the website
 """
+
+
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
 storage = FileStorage()
 storage.reload()
