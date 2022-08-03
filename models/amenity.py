356f11eb23f38bd47a963b37e10152eca1adee5e
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-03 04:45:11 -0700

    models[Amenity]: added the class amenity

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The class Amenity should inherit BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    The public class attribute should return an empty string
+    """
+    name = ""
