684a7d4c7c88bced7a486a60cb2ecd5678db38af
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-03 04:37:57 -0700

    models[city]: added a new class for city

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This class City inherits BaseModel
+"""
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    The public class attributes should be empty
+    """
+    state_id = ""
+    name = ""
