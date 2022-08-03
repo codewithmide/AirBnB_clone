50175eb5233bc853cf5be0c4bdf35284fc13a983
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-03 04:33:55 -0700

    models[state]: created another class state

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+The class State inherits BaseModel
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    the public class attribute name should be an empty string
+    """
+    name = ""
