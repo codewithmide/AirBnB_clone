bcafb0246b0c11bc5aa31dba02e42d57bb321e3c
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-03 05:08:13 -0700

    models[engine(file_storage)]: Update FileStorage to manage correctly serialization and deserialization of all our new classes: Place, State, City, Amenity and Review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,8 +5,14 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-classes = {"BaseModel": BaseModel, "User":User}
+classes = {"BaseModel": BaseModel, "User": User, "State": State,
+           "Place": Place, "City": City, "Amenity": Amenity, "Review": Review}
 
 
 class FileStorage:
