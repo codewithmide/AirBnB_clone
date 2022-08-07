35161dc739f3408c46e9e93cf1837d4bde98b3da
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-07 03:49:08 -0700

    attended to pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -41,4 +41,5 @@ class BaseModel:
 
     def __str__(self):
         """ str """
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
